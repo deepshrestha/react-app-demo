e431d6fe7e1eb3f8b16599683d946166efb0ca9d
bajracharyadjsh <djs.44@hotmail.com>
2021-05-25 13:47:22 +0545

    package versions upgraded

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,36 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        library: 'myBundle'
-    },
-    watch: true,
-    module: {
-		rules: [
-            {
-                test: /\.js?$/,
-                use: "babel-loader",
-                exclude: "/node_modules/",
-                /* options: {
-                    presets: [
-                        "@babel/preset-env",
-                        "@babel/preset-react"
-                    ]
-                } */
-            },
-            {
-                test: /\.css?$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./public/index.html"
-        })
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    library: "myBundle",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css?$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    devServer: {
-        historyApiFallback: true
-    }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    port: 1234,
+    compress: true,
+  },
+  devtool: "source-map",
 };
