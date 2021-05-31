d7c2a9464666fb5a0898df09bdba25c9aea4c538
Nina Rai <nonierai@gmail.com>
2021-05-31 11:03:03 +0545

    Post routes added. Comment route not working

--- server/controllers/Post.js
+++ server/controllers/Post.js
@@ -10,3 +10,89 @@ exports.createPost = async (req, res) => {
       res.status(500).json(err);
     }
 }
+
+//get a post
+exports.getPost = async(req,res) => {
+  try {
+      const post = await Post.findById(req.params.id)
+      res.status(200).json(post)
+  } catch (err) {
+      res.status(500).json(err)
+  }
+}
+
+//update a post
+exports.updatePost =  async (req,res) => {    
+  try {
+      const post = await Post.findById(req.params.id)
+      if (post.userId === req.body.userId ) {
+          await post.updateOne({ $set: req.body })
+          res.status(200).json("The post has been updated")
+      } else {
+          res.status(403).json("You cannot update post.")
+      }
+  } catch (err) {
+      res.status(500).json(err)
+  }    
+}
+
+
+//delete a post
+exports.deletePost =  async (req,res) => {    
+  try {
+      const post = await Post.findById(req.params.id)
+      if (post.userId === req.body.userId ) {
+          await post.deleteOne({ $set: req.body })
+          res.status(200).json("The post has been deleted.")
+      } else {
+          res.status(403).json("You can not delete  post.")
+      }
+  } catch (err) {
+      res.status(500).json(err)
+  }    
+}
+
+//show all posts in feed
+exports.getAllPosts =  async (req,res) => {
+  try{
+      const allPosts = await Post.find()
+          res.status(200).json(allPosts)
+  } catch (err) {
+      res.status(500).json(err)
+  } 
+}
+
+//like or dislike a post
+exports.likePost =  async (req,res) => {
+  try {
+      const post = await Post.findById(req.params.id)
+      if (!post.likes.includes(req.body.userId)) {
+          await post.updateOne({ $push: {likes: req.body.userId} })
+          res.status(200).json("The post has been liked.")
+      } else {
+          await post.updateOne( { $pull: {likes: req.body.userId }})
+          res.status(200).json("The post has been disliked.")
+      }
+  } catch (errr) {
+      res.status(500).json(err)
+  }
+}
+
+
+//comment on a post
+
+exports.commentPost =  async (req,res) => {
+  try {
+      const post = await Post.findById(req.params.id)
+      if (!post.likes.includes(req.body.userId)) {
+          await post.updateOne({ $push: {comments: {
+            coommentator: req.body.userId,
+            comment: req.body.comment
+          }}})
+          res.status(200).json("The comment has been posted.")
+      } 
+  } catch (errr) {
+      res.status(500).json(err)
+  }
+}
+
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,5 +2,11 @@ const router = require("express").Router()
 const PostController = require("../controllers/Post")
 
 router.post("/create", PostController.createPost)
+router.get("/:id", PostController.getPost )
+router.put("/:id", PostController.updatePost)
+router.delete("/:id", PostController.deletePost)
+router.get("/feed/all", PostController.getAllPosts)
+router.put("/:id/like", PostController.likePost)
+router.put("/:id/comment", PostController.commentPost)
 
 module.exports = router
